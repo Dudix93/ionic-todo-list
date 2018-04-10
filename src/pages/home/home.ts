4ebab659b3cea5b0c0f303c9ed361c2caaf0d3fd
Dudix93 <dodo45693@gmail.com>
2018-04-10 18:58:57 +0200

    pagination & priority sorting fixed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,34 +10,86 @@ export class HomePage {
   new_task:String = '';
   currentSorting:String = '';
   currentColSorted:String = '';
-  selectedPriority:String = 'medium';
-  
-  rowsPerPage:number = 5;
+  selectedPriority:String = 'Low';
 
   rowsPerPageList:Array<any> = [
     {'id':0, 'value':5, 'name':'5'},
     {'id':1, 'value':10, 'name':'10'},
-    {'id':2, 'value':15, 'name':'15'}];
+    {'id':2, 'value':15, 'name':'15'},
+    {'id':2, 'value':2, 'name':'2'}
+  ];
 
   priorityList:Array<any> = [
-    {'id':0, 'value':'low', 'name':'Low'},
-    {'id':1, 'value':'medium', 'name':'Medium'},
-    {'id':2, 'value':'high', 'name':'High'}];
+    {'id':0, 'value':'Low', 'name':'Low'},
+    {'id':1, 'value':'Medium', 'name':'Medium'},
+    {'id':2, 'value':'High', 'name':'High'}
+  ];
 
   todo:Array<any> = [
-    {task_name:'task1',priority:'low',done:false},
-    {task_name:'task2',priority:'low',done:true},
-    {task_name:'task3',priority:'high',done:false},
-    {task_name:'task4',priority:'low',done:false},
-    {task_name:'task5',priority:'low',done:true},
-    {task_name:'task6',priority:'high',done:false}];
+    {task_name:'task1',priority_value:1,priority:'Medium',done:false},
+    {task_name:'task2',priority_value:0,priority:'Low',done:true},
+    {task_name:'task3',priority_value:2,priority:'High',done:false},
+    {task_name:'task4',priority_value:0,priority:'Low',done:false},
+    {task_name:'task5',priority_value:0,priority:'Low',done:true},
+    {task_name:'task6',priority_value:2,priority:'High',done:false}
+  ];
+    
+  pagesList:Array<number> = [];
+  
+  rowsPerPage:number = 5;
+  firstDisplayedIndex:number = 1;
+  lastDisplayedIndex:number = 5;
+  currentPage:number = 1;
+  pages:number = Math.ceil(this.todo.length/this.rowsPerPage);
 
   constructor(public navCtrl: NavController) {
-  
+    for(let i=1;i<=this.pages;i++) this.pagesList.push(i);
+
+    this.todo.forEach(element => {
+      console.log(element);
+    });
+  }
+
+  updatePages(){
+    this.pages = Math.ceil(this.todo.length/this.rowsPerPage);
+    this.pagesList = [];
+    for(let i=1;i<=this.pages;i++) this.pagesList.push(i);
+
+    this.currentPage = 1;
+
+    this.updateTasksRange();
+  }
+
+  updateTasksRange(){
+    if(this.currentPage == 1) this.firstDisplayedIndex = 1;
+    else this.firstDisplayedIndex = ((this.currentPage-1) * this.rowsPerPage) + 1; 
+
+    if(this.currentPage == this.pages) this.lastDisplayedIndex = this.todo.length;
+    else this.lastDisplayedIndex = Number(this.firstDisplayedIndex) + Number(this.rowsPerPage) - 1;
+  }
+
+  nextPage(){
+    if(this.currentPage != this.pages){
+      this.currentPage++;
+      this.updateTasksRange();
+    }
+  }
+
+  prevPage(){
+    if(this.currentPage != 1){
+      this.currentPage--;
+      this.updateTasksRange();
+    }
   }
 
   addNewTask(){
-    this.todo.push({'id':this.priorityList.length-1, 'priority':this.selectedPriority, 'task_name':this.new_task, 'done':false});
+    this.todo.push({'id':this.priorityList.length-1, 
+                    'priority_value':this.priorityList.map(function(e) { return e.value; }).indexOf(this.selectedPriority),
+                    'priority':this.selectedPriority, 
+                    'task_name':this.new_task, 
+                    'done':false});
+    this.updatePages();
+    this.updateTasksRange();
   }
 
   compareValues(key, order='asc') {
